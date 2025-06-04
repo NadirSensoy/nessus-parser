8c16416cfa9ee7768cb466ab3c3d81ddf2f99978
Nadir Sensoy <ndr2001.ne@gmail.com>
2025-06-04 13:49:10 +0300

    Nessus Report Parser v3 - temiz kodlar

--- compare.bat
+++ compare.bat
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 @echo off
 python compare_versions.py
 exit
-=======
-@echo off
-python compare_versions.py
-exit
->>>>>>> f2cb617c794372eaa48498d769c99ed44e0a6321
--- run.bat
+++ run.bat
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 @echo off
 REM Python dosyasını çalıştırır
 
@@ -12,22 +11,3 @@ IF ERRORLEVEL 1 (
 
 REM Python scriptini çalıştır
 python script.py
-
-
-=======
-@echo off
-REM Python dosyasını çalıştırır
-
-REM Ortamda python yüklü mü kontrol et
-where python >nul 2>nul
-IF ERRORLEVEL 1 (
-    echo Python is not installed or not in PATH.
-    pause
-    exit /b
-)
-
-REM Python scriptini çalıştır
-python script.py
-
-
->>>>>>> f2cb617c794372eaa48498d769c99ed44e0a6321
